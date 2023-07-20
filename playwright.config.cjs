77a2a65478232d4bae6ceff28e03f0c26cf2c9b4
Pavdem2012 <pavdem2012@gmail.com>
2023-07-20 15:20:12 +0700

    попытка добавить блок для #google_vignette

--- playwright.config.cjs
+++ playwright.config.cjs
@@ -1,80 +1,74 @@
-// @ts-check
-const { defineConfig, devices } = require('@playwright/test')
+const { defineConfig, devices } = require('@playwright/test');
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// require('dotenv').config();
-
-/**
- * @see https://playwright.dev/docs/test-configuration
- */
 module.exports = defineConfig({
-  testDir: './src/tests/e2e',
-  /* Run tests in files in parallel */
-  fullyParallel: false,
-  /* Fail the build on CI if you accidentally left test.only in the source code. */
-  forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 3 : 3,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [['html', { outputFolder: 'reports/playwright' }]],
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
-  use: {
-    /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
-
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
-    //Capture Screenshot on failure
-    screenshot: 'only-on-failure'
-  },
-
-  /* Configure projects for major browsers */
-  projects: [
-    // {
-    //   name: 'chromium',
-    //   use: { ...devices['Desktop Chrome'] }
-    // },
-    //
-    // {
-    //   name: 'firefox',
-    //   use: { ...devices['Desktop Firefox'] }
-    // },
-
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] }
+    testDir: './src/tests/e2e',
+    fullyParallel: false,
+    forbidOnly: !!process.env.CI,
+    retries: process.env.CI ? 2 : 0,
+    workers: process.env.CI ? 3 : 3,
+    reporter: [['html', { outputFolder: 'reports/playwright' }]],
+    use: {
+        trace: 'on-first-retry',
+        screenshot: 'only-on-failure',
     },
-
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    //  {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
-  ]
-
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
-})
+    projects: [
+        {
+            name: 'chromium',
+            use: {
+                ...devices['Desktop Chrome'],
+                browserName: 'chromium',
+                contextOptions: {
+                    routes: [
+                        {
+                            // Перехватываем запросы содержащие /#google_vignette в любом месте урла
+                            url: '**/*/#google_vignette*',
+                            handler: async (route) => {
+                                // Блокируем запрос
+                                await route.abort();
+                            },
+                        },
+                    ],
+                },
+            },
+        },
+        {
+            name: 'firefox',
+            use: {
+                ...devices['Desktop Firefox'],
+                browserName: 'firefox',
+                contextOptions: {
+                    routes: [
+                        {
+                            // Перехватываем запросы содержащие /#google_vignette в любом месте урла
+                            url: '**/*/#google_vignette*',
+                            handler: async (route) => {
+                                // Блокируем запрос
+                                await route.abort();
+                            },
+                        },
+                    ],
+                },
+            },
+        },
+        {
+            name: 'webkit',
+            use: {
+                ...devices['Desktop Safari'],
+                browserName: 'webkit',
+                contextOptions: {
+                    routes: [
+                        {
+                            // Перехватываем запросы содержащие /#google_vignette в любом месте урла
+                            url: '**/*/#google_vignette*',
+                            handler: async (route) => {
+                                // Блокируем запрос
+                                await route.abort();
+                            },
+                        },
+                    ],
+                },
+            },
+        },
+        // ... другие проекты ...
+    ],
+});
