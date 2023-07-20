fccb8ee7e93a9bafd669601fd9061c8f0f8431c8
Pavdem2012 <pavdem2012@gmail.com>
2023-07-20 21:30:24 +0700

    попытка добавить adblock

--- playwright.config.cjs
+++ playwright.config.cjs
@@ -18,9 +18,9 @@ module.exports = defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 5 : 2,
+  retries: process.env.CI ? 2 : 2,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 3 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [['html', { outputFolder: 'reports/playwright' }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
@@ -54,7 +54,10 @@ module.exports = defineConfig({
 
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'] }
+      use: { ...devices['Desktop Chrome'] },
+      launchOptions: {
+        args: [`--load-extension=${join(__dirname, 'src/framework/extensions/chrome')}`],
+      }
     },
 
     // {
